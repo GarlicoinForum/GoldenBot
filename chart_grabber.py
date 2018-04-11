9c6545250238d737445967e2422d38264e656a04
JasonG-FR <jason.gombert@protonmail.com>
2018-04-11 14:51:30 +0200

    Changing chromium to chromium-browser

--- chart_grabber.py
+++ chart_grabber.py
@@ -3,13 +3,13 @@ import time
 
 def grab_chart():
     while True:
-        os.system('chromium --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=1d&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
+        os.system('chromium-browser --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=1d&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
         os.system('mv screenshot.png screenshot1.png')
-        os.system('chromium --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=1w&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
+        os.system('chromium-browser --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=1w&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
         os.system('mv screenshot.png screenshot2.png')
-        os.system('chromium --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=1m&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
+        os.system('chromium-browser --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=1m&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
         os.system('mv screenshot.png screenshot3.png')
-        os.system('chromium --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=3m&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
+        os.system('chromium-browser --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe=3m&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080')
         os.system('mv screenshot.png screenshot4.png')
         os.system('convert screenshot1.png -crop 1213x510+343+380 1d.png')
         os.system('convert screenshot2.png -crop 1213x510+343+380 1w.png')
