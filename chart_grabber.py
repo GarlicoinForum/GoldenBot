438cc93e827824b895924581e3c10c93f6fe8685
JasonG-FR <jason.gombert@protonmail.com>
2018-04-11 16:40:05 +0200

    BugFix : Index Error

--- chart_grabber.py
+++ chart_grabber.py
@@ -18,7 +18,7 @@ def grab_chart(time_range):
         with lock:  # Using lock to be sure that 2 chromium threads start at the same time because they both create a screenshot.png file
             os.system('chromium-browser --headless --disable-gpu --screenshot "https://bitscreener.com/coins/garlicoin?timeframe={}&chart_type=candle&chart_unit=usd&is_global=true" --window-size=1920,1080'.format(time_range))
             os.system('mv screenshot.png screenshot_{}.png'.format(time_range))
-            os.system('convert screenshot_{}.png -crop 1213x510+343+380 {}.png'.format(time_range))
+            os.system('convert screenshot_{0}.png -crop 1213x510+343+380 {0}.png'.format(time_range))
         time.sleep(sleeping_ranges[time_range])
 
 
